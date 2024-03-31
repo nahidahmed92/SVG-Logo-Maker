3b0d8b0440c3ef49c474dbcceb34f73be61da299
Nahid Ahmed <npahmed92@gmail.com>
2024-03-31 12:33:19 -0400

    square instance should call properties from parent

--- test/square.test.js
+++ test/square.test.js
@@ -7,5 +7,16 @@ describe('Square', () => {
 
       expect(square).toBeInstanceOf(Square);
     });
+    it('should be an instance of Square with the correct children assigned', () => {
+      const brandText = 'NPA';
+      const brandTextColor = 'white';
+      const shapeColor = 'black';
+      const square = new Square(brandText, brandTextColor, shapeColor);
+
+      expect(square).toBeInstanceOf(Square);
+      expect(square.brandText).toEqual(brandText);
+      expect(square.brandTextColor).toEqual(brandTextColor);
+      expect(square.shapeColor).toEqual(shapeColor);
+    });
   });
 });
