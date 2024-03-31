7c16c8ab54a0a14f7ae166f2af3d1f68b6dc618e
Nahid Ahmed <npahmed92@gmail.com>
2024-03-31 17:00:29 -0400

    triangle test instance should call properties from parent

--- test/triangle.test.js
+++ test/triangle.test.js
@@ -7,5 +7,16 @@ describe('Triangle', () => {
 
       expect(triangle).toBeInstanceOf(Triangle);
     });
+    it('should be an instance of Triangle with the correct children assigned', () => {
+      const brandText = 'NPA';
+      const brandTextColor = 'white';
+      const shapeColor = 'black';
+      const triangle = new Triangle(brandText, brandTextColor, shapeColor);
+
+      expect(triangle).toBeInstanceOf(Triangle);
+      expect(triangle.brandText).toEqual(brandText);
+      expect(triangle.brandTextColor).toEqual(brandTextColor);
+      expect(triangle.shapeColor).toEqual(shapeColor);
+    });
   });
 });
