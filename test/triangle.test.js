cd495adc2272ffd0e033c4c5383386b531378f16
Nahid Ahmed <npahmed92@gmail.com>
2024-03-31 18:01:39 -0400

    triangle test to match render()

--- test/triangle.test.js
+++ test/triangle.test.js
@@ -19,4 +19,22 @@ describe('Triangle', () => {
       expect(triangle.shapeColor).toEqual(shapeColor);
     });
   });
+  describe('render()', () => {
+    it('should render triangle with data', () => {
+      const brandText = 'NPA';
+      const brandTextColor = 'white';
+      const shapeColor = 'black';
+      const triangle = new Triangle(brandText, brandTextColor, shapeColor);
+
+      const expectedResults = `
+      <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+        <polygon points="140,10 20,190 280,190" fill="${shapeColor}"/>
+        <text x="150" y="140" font-size="60" text-anchor="middle" fill="${brandTextColor}">
+        ${brandText.toUpperCase()}
+        </text>
+      </svg>`;
+
+      expect(triangle.render()).toEqual(expectedResults);
+    });
+  });
 });
