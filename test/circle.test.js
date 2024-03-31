5b1879388802981c853095ff864b6738f2b0d061
Nahid Ahmed <npahmed92@gmail.com>
2024-03-31 12:24:54 -0400

    update circle test case it should be an instance... and add render test case

--- test/circle.test.js
+++ test/circle.test.js
@@ -7,12 +7,34 @@ describe('Circle', () => {
 
       expect(circle).toBeInstanceOf(Circle);
     });
-    it('should be an instance of Circle with the correct children assigned to the children string', () => {
-      const children = 'child';
-      const circle = new Circle(children);
+    it('should be an instance of Circle with the correct children assigned', () => {
+      const brandText = 'NPA';
+      const brandTextColor = 'white';
+      const shapeColor = 'black';
+      const circle = new Circle(brandText, brandTextColor, shapeColor);
 
       expect(circle).toBeInstanceOf(Circle);
-      expect(circle.children).toEqual(children);
+      expect(circle.brandText).toEqual(brandText);
+      expect(circle.brandTextColor).toEqual(brandTextColor);
+      expect(circle.shapeColor).toEqual(shapeColor);
+    });
+  });
+  describe('render()', () => {
+    it('should render circle with data', () => {
+      const brandText = 'NPA';
+      const brandTextColor = 'white';
+      const shapeColor = 'black';
+      const circle = new Circle(brandText, brandTextColor, shapeColor);
+
+      const expectedResults = `
+    <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+      <circle cx="150" cy="100" r="80" fill="${shapeColor}" />
+      <text x="150" y="125" font-size="60" text-anchor="middle" fill="${brandTextColor}">
+      ${brandText.toUpperCase()}
+      </text>
+    </svg>`;
+
+      expect(circle.render()).toEqual(expectedResults);
     });
   });
 });
